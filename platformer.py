9c4c3e1d026c61833467316d6e4bd7464276e777
kwnspir <kwnstantinosstar4@gmail.com>
2019-03-21 21:46:06 +0200

    first

--- platformer.py
+++ platformer.py
@@ -8,14 +8,52 @@ pygame.init()
 
 #size = width, height = 1920, 1080
 size = width, height = 800, 600
+
 black = 0, 0, 0
+
 #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen = pygame.display.set_mode((width, height))
+
+clock=pygame.time.Clock()
+
 tiles_num=64
+tile_width=tile_height=32
 
 FPS=60
 
+class Player():
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+		self.vx=0
+		self.vy=0
+		self.on_ground=True
+		self.health=100
+		self.mana=100
+		self.rect=(0,0,tile_width,tile_height)
+
+	def draw(self):
+		#self.rect.center=(self.x,self.y)
+		pygame.draw.rect(screen,(255,0,0),self.rect)
+
+	def collision_detect(self):
+		pass
+
+	def move(self):
+		pass
+
+player1=Player(width/2,height/2)
+
 def main():
+
+	screen.fill(black)
+
+	player1.draw()
+
+	clock.tick(FPS)
+
+	pygame.display.update()
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: 
 			sys.exit()
