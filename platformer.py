4fb9b1676d9f08ca59c6b48d87a29dec5a3bada0
mike <mikedram0@gmail.com>
2019-03-21 21:25:17 +0200

    oops

--- platformer.py
+++ platformer.py
@@ -1,1 +1,36 @@
 import pygame
+
+
+pygame.init()
+width = 1280
+height = 720
+black = (0,0,0)
+white = (255,255,255)
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+sysfont = pygame.font.SysFont(None , 40)
+
+nTilesX = 64
+nTilesY = 64
+
+
+def main():
+	
+	event_handeler()
+
+
+
+
+def event_handeler():
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: 
+			pygame.quit()
+			quit()
+
+
+while 1:
+	main()
+
+pygame.quit()
+quit()
