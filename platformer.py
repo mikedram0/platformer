a723f2d3081e830de9a557bd1a23b8ca8a7693ad
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-23 14:51:41 +0200

    Update platformer.py

--- platformer.py
+++ platformer.py
@@ -3,18 +3,22 @@ import pygame
 import time
 import math
 import random
-
 pygame.init()
+#size = width, height = 1920, 1080
+size = width, height = 32*32, 18*32
+black = 0, 0, 0
 
-size = width, height = 800, 600
-black = (0, 0, 0)
-white = (255,255,255)
 #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 mag1 = pygame.image.load("magician1.png")
+mag2 = pygame.image.load("magician2.png")
+brick_img = pygame.image.load("brick.png")
 screen = pygame.display.set_mode((width, height))
+
 clock=pygame.time.Clock()
-FPS=60
+
 staticList = []
+
+FPS=60
 
 class Player():
 	def __init__(self,x,y):
@@ -22,139 +26,150 @@ class Player():
 		self.y=y
 		self.vx=0
 		self.vy=0
+		self.on_ground=True
 		self.on_ground=False
-		self.gravity = 0.03
 		self.health=100
 		self.mana=100
+		self.rect=pygame.Rect(0,0,25,25)
+		self.dir=-1
 		self.image = mag1
 
 		self.rect=self.image.get_rect()
 
 	def draw(self):
-		self.rect.center = (self.x,self.y)		
+		global direction
+		self.rect.center = (self.x,self.y)
+
+		if self.on_ground==False:
+			self.image=mag2
+		else:
+			self.image=mag1
+
 		screen.blit(self.image,self.rect)
-		pygame.draw.rect(screen,white,self.rect,3)
 
 	def collision_detect(self):
-
 		for static in staticList:
-			if self.y+self.rect.height/2 < static.y+static.rect.height/2:
-				if self.rect.colliderect(static.rect):
-					self.y-=self.vy
-					self.vy=0
-					self.y=static.y-static.rect.height/2-self.rect.height/2
-					self.on_ground=True
-				
+			if self.rect.bottom>=static.rect.top:
+				self.on_ground=True
+				self.vy=0
+				self.rect.bottom=static.rect.top
+				pygame.draw.rect(screen,(255,255,255),self.rect,2)
+				print(self.y)
+
 
 	def move(self):
-		
-		self.vy += self.gravity
+		print(self.vy)
+		if self.y+self.rect.height/2 >= height:
+			self.vy=0
+			self.y=height-25/2
+		'''
+		if not self.on_ground: 
+			self.vy+=0.01
+			if self.y+self.rect.height/2 >= height:
+				self.on_ground = True
+				self.y=height-32/2
+				self.vy=0
+	'''
+		self.vy += 0.03
+
+		#if self.on_ground:
 		if self.y + self.rect.height + self.vy >= height:
 
 			self.on_ground = True
 			self.vy = 0
 			self.y = height-self.rect.height
-			
+
 		else:
 			self.on_ground = False
-	
+
+		if self.on_ground==True:
+			self.vy = 0
+
+
+		self.vy+=0.01
+
 		self.x+=self.vx
 		self.y+=self.vy
 
+
 class Static:
 
-	def __init__(self,x,y,width,height):
+	def __init__(self,x,y):
 		self.x = x
 		self.y = y
-		self.rect = pygame.Rect(0,0,width,height)
-		self.rect.center = (self.x,self.y)
+		self.rect = brick_img.get_rect()
 		staticList.append(self)
 
 	def draw(self):
-
 		self.rect.center = (self.x,self.y)
-		pygame.draw.rect(screen,white,self.rect)
+		screen.blit(brick_img,self.rect)
+		pygame.draw.rect(screen,(255,255,255),self.rect,2)
 
-
-
-
-
-
-
-player1=Player(width/2,height/2)
-block1 = Static(width/2,height - 100 , 500 , 10)
+static1=Static(width/2,height/2)
+player1=Player(width/2,height/2-150)
 
 def main():
+	global direction,mag1,mag2
 
 	clock.tick(FPS)
-	dt = clock.get_fps()
-
-	event_handeler()
-
 	screen.fill(black)
-
 	player1.collision_detect()
 	player1.move()
 	player1.draw()
-
 	for static in staticList:
 		static.draw()
 
-
 	pygame.display.update()
-
 	
-
-def event_handeler():
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: 
 			sys.exit()
-
-
 		if event.type == pygame.KEYUP:
 			if event.key==pygame.K_a:
 				player1.vx=0
-
 			if event.key==pygame.K_SPACE:
 				pass
-
 			if event.key==pygame.K_LSHIFT:
 				pass
-
 			if event.key==pygame.K_d:
 				player1.vx=0
-
 			if event.key==pygame.K_s:
 				pass
-
 			if event.key == pygame.K_w:
 				pass
-
 		if event.type == pygame.KEYDOWN:
 			
 			if event.key==pygame.K_ESCAPE:
 				sys.exit()
 
 			if event.key==pygame.K_a:
+				tmp=player1.dir
+				player1.dir=-1
 				player1.vx=-2
-				
+				if tmp*player1.dir<0:
+					mag2=pygame.transform.flip(mag2,True,False)
+					mag1=pygame.transform.flip(mag1,True,False)
 
 			if event.key==pygame.K_SPACE:
 				pass
-
 			if event.key==pygame.K_LSHIFT:
 				pass
 
 			if event.key==pygame.K_d:
+				tmp=player1.dir
+				player1.dir=1
 				player1.vx=2
-				
+				if tmp*player1.dir<0:
+					mag2=pygame.transform.flip(mag2,True,False)
+					mag1=pygame.transform.flip(mag1,True,False)
+
 			if event.key==pygame.K_s:
 				pass
 
 			if event.key == pygame.K_w:
+				player1.vy=-4
 				#player1.on_ground = False
 				if player1.on_ground:
-					player1.y=player1.y-50
 					player1.vy=-2
 				#player1.on_ground = False
 				#player1.on_ground = True
